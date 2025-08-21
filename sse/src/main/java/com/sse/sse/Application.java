6802969e35f9b0a2155f85f2d7b2ae143bbd70f8
김민지 <ghi512151@gmail.com>
2025-08-21 17:23:16 +0900

    delete: #6 - Application 테스트 코드 제거

--- sse/src/main/java/com/sse/sse/Application.java
+++ sse/src/main/java/com/sse/sse/Application.java
@@ -1,10 +1,6 @@
 package com.sse.sse;
 
-import com.sse.sse.controller.MemberController;
-
 public class Application {
     public static void main(String[] args) {
-        MemberController controller = new MemberController();
-        controller.signup();
     }
 }
